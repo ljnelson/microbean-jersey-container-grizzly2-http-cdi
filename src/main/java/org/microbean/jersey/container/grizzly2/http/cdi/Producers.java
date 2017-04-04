27e40030569d63aa4642779810f274979878eab8
Laird Nelson <ljnelson@gmail.com>
2017-04-04 16:42:41 -0700

    Version refresh and documentation update

--- src/main/java/org/microbean/jersey/container/grizzly2/http/cdi/Producers.java
+++ src/main/java/org/microbean/jersey/container/grizzly2/http/cdi/Producers.java
@@ -46,20 +46,68 @@ import org.slf4j.LoggerFactory;
  * A class housing <a
  * href="http://docs.jboss.org/cdi/spec/2.0.EDR2/cdi-spec.html#producer_method">producer
  * methods</a> that manufacture certain Grizzly- and Jersey-related
- * objects.
+ * objects, including {@link HttpServer} instances (by way of {@link
+ * GrizzlyHttpServerFactory} instances) and {@link
+ * GrizzlyHttpContainer} instances (by way of {@link ContainerFactory}
+ * instances).
  *
- * @author <a href="http://about.me/lairdnelson"
+ * <p>For this class to be useful, a CDI container must be capable of
+ * locating or otherwise supplying {@link SSLEngineConfigurator}
+ * instances.  See the <a
+ * href="https://ljnelson.github.io/microbean-grizzly-http-server-cdi/"
+ * target="_parent">MicroBean Grizzly HTTP Server CDI Integration</a>
+ * project for an example of a project that supplies such
+ * instances.</p>
+ *
+ * @author <a href="https://about.me/lairdnelson"
  * target="_parent">Laird Nelson</a>
  */
 @ApplicationScoped
 class Producers {
 
-  private static final Logger logger = LoggerFactory.getLogger(Producers.class);
+
+  /*
+   * Static fields.
+   */
   
+
+  /**
+   * A {@link Logger} for use by instances of this class.
+   *
+   * <p>This field is never {@code null}.</p>
+   *
+   * @see LoggerFactory#getLogger(Class)
+   */
+  private static final Logger logger = LoggerFactory.getLogger(Producers.class);
+
+
+  /*
+   * Constructors.
+   */
+
+  
+  /**
+   * Creates a new {@link Producers}.
+   */
   private Producers() {
     super();
   }
-  
+
+  /**
+   * Creates and returns a new {@link GrizzlyHttpContainer} if the
+   * supplied {@link Instance} containing {@link Application}
+   * instances is {@link Instance#isUnsatisfied() not unsatisfied}.
+   *
+   * <p>This method may return {@code null}.</p>
+   *
+   * @param applicationInstance an {@link Instance} {@linkplain
+   * Instance#get() housing} an {@link Application}; may be {@code
+   * null}
+   *
+   * @return a {@link GrizzlyHttpContainer}, or {@code null}
+   *
+   * @see ContainerFactory#createContainer(Class, Application)
+   */
   @Produces
   @Dependent
   private static final GrizzlyHttpContainer produceGrizzlyHttpContainer(final Instance<Application> applicationInstance) {
@@ -78,10 +126,56 @@ class Producers {
     return returnValue;
   }
 
+  /**
+   * Creates and returns a new {@link HttpServer} if the supplied
+   * {@link Instance} containing a {@link GrizzlyHttpContainer} is
+   * non-{@code null} and {@link Instance#isUnsatisfied() not
+   * unsatisfied}.
+   *
+   * <p>This method may return {@code null}.</p>
+   *
+   * @param host the address of the network interface to listen on;
+   * may be {@code null} in which case {@code 0.0.0.0} will be used
+   * instead
+   *
+   * @param port the port to listen on; {@code 80} by default
+   *
+   * @param contextPath where the resulting web application should be
+   * rooted; may be {@code null} in which case {@code /} will be used
+   * instead
+   *
+   * @param handlerInstance an {@link Instance} {@linkplain
+   * Instance#get() housing} a {@link GrizzlyHttpContainer} instance
+   * (such as might be produced by the {@link
+   * #produceGrizzlyHttpContainer(Instance)} method); may be {@code
+   * null} or {@linkplain Instance#isUnsatisfied() unsatisfied} in
+   * which case {@code null} will be returned
+   *
+   * @param secure whether the communication with the server will be
+   * secure or not
+   *
+   * @param sslEngineConfiguratorInstance an {@link Instance}
+   * {@linkplain Instance#get() housing} an {@link
+   * SSLEngineConfigurator}; may be {@code null} or {@linkplain
+   * Instance#isUnsatisfied() unsatisfied} in which case an {@link
+   * SSLEngineConfigurator} instance will not be passed to the {@link
+   * GrizzlyHttpServerFactory#createHttpServer(URI,
+   * GrizzlyHttpContainer, boolean, SSLEngineConfigurator, boolean)}
+   * method
+   *
+   * @return an {@link HttpServer}, or {@code null}
+   *
+   * @see #produceGrizzlyHttpContainer(Instance)
+   *
+   * @see GrizzlyHttpContainer
+   *
+   * @see GrizzlyHttpServerFactory#createHttpServer(URI,
+   * GrizzlyHttpContainer, boolean, SSLEngineConfigurator, boolean)
+   */
   @Produces
   @Dependent
   private static final HttpServer produceHttpServer(@ConfigurationValue(value = "host", defaultValue = "0.0.0.0") final String host,
-                                                    @ConfigurationValue(value = "port", defaultValue = "8080") final int port,
+                                                    @ConfigurationValue(value = "port", defaultValue = "80") final int port,
                                                     @ConfigurationValue(value = "contextPath", defaultValue = "/") final String contextPath,
                                                     final Instance<GrizzlyHttpContainer> handlerInstance,
                                                     @ConfigurationValue("secure") final boolean secure,
